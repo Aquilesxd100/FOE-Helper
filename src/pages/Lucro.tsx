1c45b119964020803082adf2502941fb0ba207ad
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-01-03 12:15:01 -0300

    Ajustes

--- src/pages/Lucro.tsx
+++ src/pages/Lucro.tsx
@@ -1,22 +1,55 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Link } from "react-router-dom";
-import { Painel } from "../style";
-import { Botoes } from "../style";
-import { Botao } from "../style";
+import { Painel, Botoes, Botao, TituloPainel, PainelRodape, Formulario } from "../style";
 function Lucro () {
-    /* const [infos, lucro] = useState([]); */
+    const inputTotal : any = useRef();
+    const inputInvestido : any = useRef();
+    const inputLucroBase : any = useRef();
+    const inputLucroArco : any = useRef();
+    const inputInvasor : any = useRef();
+    const resultado : any = useRef();
+    window.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const pfsTotais : number = Number(inputTotal.current.value);
+        const pfsInvestidos : number = Number(inputInvestido.current.value);
+        const lucroBase : number = Number(inputLucroBase.current.value);
+        const lucroArco : number = Number(inputLucroArco.current.value) || 0;
+        const invasor : number = Number(inputInvasor.current.value) || 0;
+        const investimento : number = Math.round(((pfsTotais - pfsInvestidos) - invasor) * 0.5 + 1 + invasor);
+        const lucro : number = Math.round((lucroBase + lucroArco) - investimento);
+        if (pfsTotais - pfsInvestidos <= invasor) {
+            resultado.current.innerText = `Não é possível ultrapassar o invasor!`;
+            return;
+        }
+        if (lucro > 0) {
+            resultado.current.innerText = `Investindo ${investimento}, você terá um lucro de ${lucro} pontos forge!`;
+        }
+        else if (lucro === 0) {
+            resultado.current.innerText = `Investindo ${investimento}, você não terá nenhum lucro além das plantas`;
+        }
+        else {
+            resultado.current.innerText = `Investindo ${investimento}, você terá um prejuízo de ${Math.abs(lucro)} pontos forge!`;
+        }
+    });
+
     return(
-        <>
+        <Formulario>
+            <TituloPainel><h1>Calculadora de Lucro</h1></TituloPainel>
             <Painel>
-            
+                <input type="number" ref={inputTotal} required placeholder="PFs totais do GE"></input>
+                <input type="number" ref={inputInvestido} required placeholder="PFs investidos no GE"></input>
+                <input type="number" ref={inputLucroBase} required placeholder="Lucro Base"></input>
+                <input type="number" ref={inputLucroArco} placeholder="Lucro do Arco"></input>
+                <input type="number" ref={inputInvasor} placeholder="Investimento de Invasor"></input>
             </Painel>
+            <PainelRodape><p ref={resultado}>Preencha os campos e pressione confirmar para continuar.</p></PainelRodape>
             <Botoes>
                 <Botao confirmacao><h3>Confirmar</h3></Botao>
                 <Link to="/home">
                     <Botao><h3>Voltar</h3></Botao>
                 </Link>
             </Botoes>
-        </>
+        </Formulario>
     );
 }
 export default Lucro;
--- src/style/index.ts
+++ src/style/index.ts
@@ -37,7 +37,6 @@ export const Card = styled.div`
         margin: 0;
     }
     h2 {
-        font-family: friz-quadrata;
         font-weight: bold;
         font-size: 22px;
         text-shadow: 2px 0 0 rgb(0, 0, 0), -2px 0 0 rgb(0, 0, 0), 0 2px 0 rgb(0, 0, 0), 0 -2px 0 rgb(0, 0, 0), 1.5px 1.5px rgb(0, 0, 0), -1.5px -1.5px 0 rgb(0, 0, 0), 1.5px -1.5px 0 rgb(0, 0, 0), -1.5px 1.5px 0 rgb(0, 0, 0);
@@ -55,17 +54,71 @@ export const CardDescricao = styled.div`
     margin: 0;
     border-top: 5px solid #000000;
 `;
-export const Painel = styled.div`
-    height: 40vh;
+export const Formulario = styled.form`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+export const TituloPainel = styled.div`
     width: 70%;
-    border: 5px solid;
+    padding: 5px 0;
+    background-color: rgb(221, 221, 221);
+    text-align: center;
+    border-top: 5px solid #000000;
+    border-right: 5px solid #000000;
+    border-left: 5px solid #000000;
+    border-bottom: 4px solid #000000;
+    h1 {
+        font-size: 26px;
+        color: #FEE351;
+        text-shadow: 2px 0 0 rgb(0, 0, 0), -2px 0 0 rgb(0, 0, 0), 0 2px 0 rgb(0, 0, 0), 0 -2px 0 rgb(0, 0, 0), 1.5px 1.5px rgb(0, 0, 0), -1.5px -1.5px 0 rgb(0, 0, 0), 1.5px -1.5px 0 rgb(0, 0, 0), -1.5px 1.5px 0 rgb(0, 0, 0);
+        letter-spacing: 1.5px;
+    }
+`;
+export const Painel = styled.div`
+    width: 70%;
+    border-left: 5px solid;
+    border-right: 5px solid;
     background-color: rgba(221, 221, 221, 0.85);
+    text-align: center;
+    padding: 15px;
+    input {
+        width: 40%;
+        text-align: center;
+        margin: 6px;
+        font-size: 18px;
+        padding: 8px;
+        border: 2px solid #000000;
+        border-radius: 5px;
+        background-color: #EEEEEE;
+    }
+    input::-webkit-outer-spin-button, input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+`;
+export const PainelRodape = styled.div`
+    width: 70%;
+    background-color: rgb(221, 221, 221);
+    border-top: 4px solid #000000;
+    border-bottom: 5px solid #000000;
+    border-left: 5px solid #000000;
+    border-right: 5px solid #000000;
+    text-align: center;
+    padding: 10px;
+    p {
+        font-size: 22px;
+        color: #FFEA5F;
+        font-weight: bolder;
+        text-shadow: 1.4px 0 0 rgb(0, 0, 0), -1.4px 0 0 rgb(0, 0, 0), 0 1.4px 0 rgb(0, 0, 0), 0 -1.4px 0 rgb(0, 0, 0), 0.8px 0.8px rgb(0, 0, 0), -0.8px -0.8px 0 rgb(0, 0, 0), 0.8px -0.8px 0 rgb(0, 0, 0), -0.8px 0.8px 0 rgb(0, 0, 0);
+        letter-spacing: 1.6px;
+    }
 `;
 export const Botoes = styled.div`
     display: inline-block;
     text-align: center;
     h3 {
-        font-family: friz-quadrata;
         font-weight: bold;
         color: #FFFFFF;
         font-size: 22px;
@@ -76,7 +129,7 @@ export const Botoes = styled.div`
 export const Botao = styled.button<any>`
     margin: 10px 15px;
     padding: 10px 0;
-    min-width: 280px;
+    width: 27vw;
     border: solid black 3px;
     border-radius: 5px;
     background-color: ${props => props.confirmacao ? "#00AE00" : "#AE0000"};
